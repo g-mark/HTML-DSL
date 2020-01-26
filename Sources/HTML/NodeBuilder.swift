a719143547116c7b2f74d955138a9575f0aa7627
Steven Grosmark <steve@g-mark.com>
2020-01-26 10:25:08 -0500

    Add support for compact node rendering - changed element names into an enum to remove stringy-ness - moved rendering into a separate struct for better control over state - removed `trim` Node type, for better control over pretty vs. compact rendering - moved element global funcs into different file

--- Sources/HTML/NodeBuilder.swift
+++ Sources/HTML/NodeBuilder.swift
@@ -1,29 +1,38 @@
 import Foundation
 
 public protocol NodeConvertible {
+    
     func asNode() -> Node
+    
 }
 
 extension Node: NodeConvertible {
+    
     public func asNode() -> Node {
         self
     }
+    
 }
 
 extension String: NodeConvertible {
+    
     public func asNode() -> Node {
         .text(self)
     }
+    
 }
 
 extension Array: NodeConvertible where Element: NodeConvertible {
+    
     public func asNode() -> Node {
         .fragment(children: map { $0.asNode() })
     }
+    
 }
 
 @_functionBuilder
 public struct NodeBuilder {
+    
     public static func buildBlock(_ components: Node...) -> Node {
         .fragment(children: components)
     }
@@ -39,10 +48,13 @@ public struct NodeBuilder {
     public static func buildEither(second: Node) -> Node {
         second
     }
+    
 }
 
 extension NodeBuilder {
+    
     public static func buildBlock(_ components: NodeConvertible...) -> Node {
         .fragment(children: components.map { $0.asNode() })
     }
+    
 }
